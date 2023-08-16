0c761537dd6dce5ddfec6e3a0e9ef6c8d808b501
Vaibhav Sharma <asvaibhavsharma@gmail.com>
2023-08-17 02:01:42 +0530

    ptani

--- selfappraisal/views.py
+++ selfappraisal/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'html/form-layout-1.html', {})
+    return render(request, 'html/index.html', {})
     # return render(request, 'form/base.html', {})

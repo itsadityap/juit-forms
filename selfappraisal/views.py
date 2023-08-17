80e036749f7478a7497e1278cb1e870716b0289d
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-08-17 22:03:34 +0530

    added html

--- selfappraisal/views.py
+++ selfappraisal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'selfappraisal/form.html', {})
+    return render(request, 'selfappraisal/forms.html', {})

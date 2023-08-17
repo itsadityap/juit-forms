3b0fbc36bb945e07814442a10131091d6f21e11c
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-08-17 21:41:16 +0530

    base updated

--- selfappraisal/views.py
+++ selfappraisal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'selfappraisal/dashboard/dashboard.html', {})
+    return render(request, 'selfappraisal/form.html', {})

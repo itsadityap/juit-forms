507e541a3b579eed7c04b2bd0c625be49be1bf42
Vaibhav Sharma <asvaibhavsharma@gmail.com>
2023-08-17 18:47:19 +0530

    incomplete

--- selfappraisal/views.py
+++ selfappraisal/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'html/index.html', {})
-    # return render(request, 'form/base.html', {})
+    return render(request, 'selfappraisal/dashboard/dashboard.html', {})

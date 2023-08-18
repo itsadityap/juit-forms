a4d32d5de15acfd2f2ddd7b6883bc3c9015612e1
Rishabh Kesarwani <rkesari2002@gmail.com>
2023-08-18 10:57:40 +0530

    up.sh -r

--- up.sh
+++ up.sh
@@ -5,3 +5,8 @@ python manage.py migrate
 if [[ "$1" == "-i" ]]; then
     python manage.py createsuperuser --username vaibhav --email vaibhav@example.com
 fi
+
+if [[ "$1" == "-r" ]]; then
+    pip freeze > requirements.txt
+    pip install -r requirements.txt
+fi

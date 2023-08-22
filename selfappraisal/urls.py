8c69c939359c2740282abc47e2d5dc0d7a01eb6c
Vaibhav Sharma <asvaibhavsharma@gmail.com>
2023-08-22 17:29:52 +0530

    fix

--- selfappraisal/urls.py
+++ selfappraisal/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('form/', views.SelfAppraisalFormCreateView.as_view(), name='createmainform'),
     path('form/<int:pk>/update', views.SelfAppraisalFormUpdateView.as_view(), name='updatemainform'),
     path('form/<int:pk>/', views.form_dashboard_view, name='formdashboard'),
+    path('form/<int:pk>/event/create/', views.EventCreateView.as_view(), name="createevent"),
+    path('form/<int:pk>/event/<int:event_id>/update/', views.EventUpdateView.as_view(), name="update-event"),
+    path('form/<int:pk>/event/<int:event_id>/delete/', views.delete_event_view, name="delete-event"),
 
-
-    
 ]
--- selfappraisal/views.py
+++ selfappraisal/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from selfappraisal.form import SelfAppraisalFormModelFormMain, EventModelForm
 from selfappraisal.models import SelfAppraisalForm, Event
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import get_object_or_404
+from django.http import Http404, HttpResponseRedirect
 
 # Create your views here.
 def home_view(request):
@@ -35,26 +37,52 @@ class SelfAppraisalFormUpdateView(UpdateView):
 
     def get_success_url(self):
         # Use self.object to access the updated form instance
-        return reverse_lazy("home", kwargs={'pk': self.object.pk})
-
+        return reverse_lazy("formdashboard", kwargs={'pk': self.object.pk})
 
 class EventCreateView(CreateView):
     model = Event
     form_class = EventModelForm
-    template_name = 'selfappraisal/form/form.html'
-    success_message = "Form updated successfully"
+    template_name = 'selfappraisal/form/create_event.html'
+    success_message = "Hello" # TODO: ADD
 
     def form_valid(self, form):
-        form.instance.name = self.request.user
+        # TODO: ADD PROTECTION
+        form.instance.form = SelfAppraisalForm.objects.get(pk=self.kwargs['pk'])
         return super().form_valid(form)
 
     def get_success_url(self):
         # Use self.object to access the updated form instance
-        return reverse_lazy("home", kwargs={'pk': self.object.pk})
+        return reverse_lazy("formdashboard", kwargs={'pk': self.kwargs['pk']})
 
+class EventUpdateView(UpdateView):
+    model = Event
+    form_class = EventModelForm
+    template_name = 'selfappraisal/form/create_event.html'
+    success_message = "Hello" # TODO: ADD
+    
+    def get_object(self, queryset=None):
+        event_id = self.kwargs.get('event_id')
+        event = get_object_or_404(Event, id=event_id)
+
+        if event.form.id != self.kwargs['pk']:
+            raise Http404("No matches the given query.")
+
+        return event
+    
+    def get_success_url(self):
+        # Use self.object to access the updated form instance
+        return reverse_lazy("formdashboard", kwargs={'pk': self.kwargs['pk']})
+
+# TODO: @rishabh fix this 
+def delete_event_view(request, pk, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    if event.form.id == pk:
+        event.delete()
+    formdashboard_url = reverse("formdashboard", kwargs={"pk": pk})
+    return HttpResponseRedirect(formdashboard_url)
 
 def form_dashboard_view(request, pk):
     mainform_obj = SelfAppraisalForm.objects.get(pk=pk)
-
+    events = Event.objects.filter(form=mainform_obj)
     # create fo
-    return render(request, 'selfappraisal/form/form_dashboard.html', {'form': mainform_obj})
+    return render(request, 'selfappraisal/form/form_dashboard.html', {'form': mainform_obj, 'events': events})
